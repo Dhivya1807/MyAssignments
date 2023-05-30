e76db3027413e9fa40a5fae2e2eb67d185672689
Dhivya1807 <133914419+Dhivya1807@users.noreply.github.com>
2023-05-30 22:20:05 +0530

    Add files via upload
    Myassignments.week3.day2

---
+++ week3/day2/inheritance/Computer.java
@@ -0,0 +1,14 @@
+package week3.day2.inheritance;
+
+//Class:Computer
+public class Computer {
+
+//  Methods   :computerModel()
+	
+	public void computerModel() {
+		System.out.println("My Computer model is windows11");
+	}
+
+
+	
+}
